213b3fd1284996b555d3a48530cf27d9c416a337
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-05-18 10:42:37 -0700

    Astrology App
    Added demo modal

--- app.js
+++ app.js
@@ -1,4 +1,6 @@
-var toastElList = [].slice.call(document.querySelectorAll('.toast'))
-var toastList = toastElList.map(function (toastEl) {
-  return new bootstrap.Toast(toastEl, option)
+var myModal = document.getElementById('myModal')
+var myInput = document.getElementById('myInput')
+
+myModal.addEventListener('shown.bs.modal', function () {
+  myInput.focus()
 })
