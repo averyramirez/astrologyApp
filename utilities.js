1633e4bf0df307db406de5f8cc6104aa44ac322e
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-09-21 21:03:25 -0700

    Update utilities.js
    moved background image function

--- utilities.js
+++ utilities.js
@@ -4,14 +4,3 @@ var myInput = document.getElementById('myInput')
 myModal.addEventListener('shown.bs.modal', function () {
   myInput.focus()
 })
-
-
-{
-  function changeImg(imgNumber)	{
-  var myImages = [".images/palms.jpg", ".images/forest.jpg", ".images/ice.jpg", ".images/woods.jpg"]; 
-  var imgShown = document.body.style.backgroundImage;
-  var newImgNumber =Math.floor(Math.random()*myImages.length);
-  document.body.style.backgroundImage = 'url('+myImages[newImgNumber]+')';
-}
-window.onload=changeImg;
-}
