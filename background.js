1dd7d40d07aef9ac426ed5f2e0d33d7a53e96334
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-09-28 15:03:52 -0700

    Update background.js
    Added background images

--- background.js
+++ background.js
@@ -1,17 +1,23 @@
 var images = [
     //Woods
     'https://images.unsplash.com/photo-1502252430442-aac78f397426?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    // //Pink Sunset
-    // 'https://images.unsplash.com/photo-1533150783171-ce47d5c2b6ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    // //Blue Mountains
-    // 'https://images.unsplash.com/photo-1491466424936-e304919aada7?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80',
-    // //Mushroom 
-    // 'https://images.unsplash.com/photo-1505820013142-f86a3439c5b2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1171&q=80',
-    // //Purple Night
-    // 'https://images.unsplash.com/photo-1498026474556-93048b8493d8?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1169&q=80',
-    // //Cityscape
-    // 'https://images.unsplash.com/photo-1546083381-2bed38b42cac?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80'
-    ];
+    //Pink Sunset
+    'https://images.unsplash.com/photo-1533150783171-ce47d5c2b6ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
+    //Dark Woods
+    'https://images.unsplash.com/photo-1605580556856-db8fae94b658?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
+    //Green Mountains
+    'https://images.unsplash.com/flagged/photo-1551301622-6fa51afe75a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
+    //Purple Trees
+    'https://images.unsplash.com/photo-1564473185935-58113cba1e80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
+    //Red Mountains
+    'https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
+    //Boston
+    'https://images.unsplash.com/photo-1632148734094-0017109ff3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2060&q=80',
+    //Port
+    'https://images.unsplash.com/photo-1632175497777-b634a3536237?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    //Lake House
+    'https://images.unsplash.com/photo-1630140963003-45211be72a0b?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1931&q=80',
+];
 
     var n = Math.floor(Math.random()*images.length);
     document.body.style.backgroundImage = 'url(' + images[n] + ')';
