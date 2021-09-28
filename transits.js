106f2acf72c764143a7ac5462ebf4acd134f02df
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2021-09-28 13:17:24 -0700

    Astrology App
    Merged moon and mercury files into one transits file

--- transits.js
+++ transits.js
@@ -1,4 +1,7 @@
-const moonPhase = {
+//Moon Phase
+
+{
+    const moonPhase = {
     url: 'https://api.farmsense.net/v1/moonphases/?d=',
     time: Math.floor(Date.now() / 1000)
 }
@@ -72,4 +75,34 @@ async function getMoon(){
 }
 
 getMoon();
+}
 
+//Mercury Retrograde
+{
+    const mercuryRetrograde = {
+    url: 'https://mercuryretrogradeapi.com?date=',
+    date: new Date().toISOString().slice(0, 10),
+}
+
+const {url, date} = mercuryRetrograde
+const isRetrograde = `${url}${date}`
+
+async function getMercury(){
+    const response = await fetch(isRetrograde);
+    const data = await response.json();
+    if(data["is_retrograde"] === true){
+        var img = new Image(); 
+        var div = document.getElementById('mercurySymbol'); 
+        img.onload = function() { 
+        div.appendChild(img); 
+    }; 
+    img.src = 'mercury.png';
+    document.getElementById('mercuryRetrograde').innerText = `Mercury is currently in Retrograde.`;
+    }
+    else {
+        console.log("Mercury is not currently in Retrograde.")
+    }
+}
+
+getMercury();
+}
