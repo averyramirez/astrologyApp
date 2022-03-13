aee07332869855a9ad6c1c5d51f11a5763e627c0
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2022-03-13 14:40:50 -0700

    Astrology App
    Removed background image cycling

--- background.js
+++ background.js
@@ -1,21 +1,16 @@
 var images = [
     //Woods
     'https://images.unsplash.com/photo-1502252430442-aac78f397426?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1170&q=80',
-    //Pink Sunset
-    'https://images.unsplash.com/photo-1533150783171-ce47d5c2b6ef?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1170&q=80',
-    //Dark Woods
-    'https://images.unsplash.com/photo-1605580556856-db8fae94b658?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
-    //Green Mountains
-    'https://images.unsplash.com/flagged/photo-1551301622-6fa51afe75a9?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
     //Purple Trees
     'https://images.unsplash.com/photo-1564473185935-58113cba1e80?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
     //Red Mountains
     'https://images.unsplash.com/photo-1477346611705-65d1883cee1e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2070&q=80',
-    //Boston
-    'https://images.unsplash.com/photo-1632148734094-0017109ff3a2?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2060&q=80',
-    //Port
-    'https://images.unsplash.com/photo-1632175497777-b634a3536237?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    //Purple Mountains
+    'https://images.unsplash.com/photo-1511300636408-a63a89df3482?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
+    //Moon
+    'https://images.unsplash.com/photo-1521893618136-49740c7dd8c6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=2070&q=80',
 ];
 
     var n = Math.floor(Math.random()*images.length);
     document.body.style.backgroundImage = 'url(' + images[n] + ')';
+
