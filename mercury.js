58904689e5404682c174b2dd754b97ba08c1cf2b
Avery Ramirez <68864812+averyramirez@users.noreply.github.com>
2022-03-28 20:17:29 -0700

    Astrology App
    Added moon and mercury transits back into the page

--- mercury.js
+++ mercury.js
@@ -11,16 +11,17 @@ async function getMercury(){
     const response = await fetch(isRetrograde);
     const data = await response.json();
     if(data["is_retrograde"] === true){
-        var img = new Image(); 
-        var div = document.getElementById('mercurySymbol'); 
-        img.onload = function() { 
-        div.appendChild(img); 
-    }; 
-    img.src = 'mercury.png';
+    //     var img = new Image(); 
+    //     var div = document.getElementById('mercurySymbol'); 
+    //     img.onload = function() { 
+    //     div.appendChild(img); 
+    // }; 
+    // img.src = 'mercury.png';
     document.getElementById('mercuryRetrograde').innerText = `Mercury is currently in retrograde. Tread carefully.`;
     // console.log(data)
     }
     else {
+        document.getElementById('mercuryRetrograde').innerText = `Mercury is not currently in retrograde.`;
         console.log("Mercury is not currently in Retrograde.")
     }
 }
